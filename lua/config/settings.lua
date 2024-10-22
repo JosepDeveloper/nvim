4d7b7c7d7d119a550b8a8f018db9732004091030
Jose Manuel Oviedo <thejm1133@gmail.com>
2024-10-22 15:31:07 -0400

    Add comments and change leader

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,7 +1,9 @@
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.tabstop = 2
+vim.o.number = true -- Habilita los numeros de la lineas
+vim.o.relativenumber = true -- Habilita los numeros relativos 
+-- Indica es espaciado sea 2
+vim.o.tabstop = 2 
 vim.o.shiftwidth = 2
-vim.o.expandtab = true
+vim.o.expandtab = true -- Habilita las tabulaciones con espacios
 vim.o.termguicolors = true
+vim.g.mapleader = ' '
 
